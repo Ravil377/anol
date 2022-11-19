4962df299743d17bf8605496fd82822c49f520cc
Ravil Shamilov <75186385+Ravil377@users.noreply.github.com>
2022-11-19 16:05:22 +0300

    подберем аналоги

--- src/components/industries/industries.js
+++ src/components/industries/industries.js
@@ -1,17 +1,40 @@
 import Swiper, { Pagination, Navigation } from "swiper";
 Swiper.use([Pagination, Navigation]);
+import enquire from 'enquire.js'
 
 const industries = '.industries__slider-js ';
+var industriesSlider;
+var indSlide = document.querySelector('.industries__wrapper-js');
 
-if(document.querySelector(industries)) {
-    const industriesSlider = new Swiper(industries, {
-        slidesPerView: 'auto',
-        centeredSlides: true,
-        loop: true,
-        initialSlide: 1,
-        navigation: {
-            nextEl: '.ourwork__next-js',
-            prevEl: '.ourwork__prev-js',
-        },
-    })
+const openHref = (e) => {
+    const target = e.target;
+    if(target.classList.contains('industries__slide')) {
+        location.href=target.dataset.href;
+    }
 }
+
+indSlide.addEventListener('click', openHref);
+
+enquire.register('screen and (min-width: 768px)', {
+	match() {
+        if(document.querySelector(industries)) {
+            indSlide.removeEventListener('click', openHref);
+
+            industriesSlider = new Swiper(industries, {
+                slidesPerView: 'auto',
+                loop: true,
+                centeredSlides: true,
+                centeredSlidesBounds: true,
+                navigation: {
+                    nextEl: '.industries__button-next-js',
+                    prevEl: '.industries__button-prev-js',
+                },
+            })
+        }
+
+	},
+	unmatch() {
+		industriesSlider.destroy(false, true);
+        indSlide.addEventListener('click', openHref);
+	}
+})
